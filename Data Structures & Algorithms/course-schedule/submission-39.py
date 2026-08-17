3f3e273d5198c12fbb39d9d698ca2c8daa36778b
chobo <39817074+chobo-zz@users.noreply.github.com>
2026-08-16 17:05:24 -0700

    Add: course-schedule - submission-39

---
+++ Data Structures & Algorithms/course-schedule/submission-39.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adjList = defaultdict(list)
+        indegree = [0] * numCourses
+
+        for crs, pre in prerequisites:
+            indegree[crs] += 1
+            adjList[pre].append(crs)
+        
+        q = deque([i for i in range(numCourses) if indegree[i] == 0])
+
+        processed = 0
+        while q:
+            crs = q.popleft()
+            processed += 1
+
+            for nextCrs in adjList[crs]:
+                indegree[nextCrs] -= 1
+                if indegree[nextCrs] == 0:
+                    q.append(nextCrs)
+                    
+        
+        return processed == numCourses
+                
+            
+        
+
