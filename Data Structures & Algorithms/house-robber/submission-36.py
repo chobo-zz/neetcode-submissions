b85abbef4cc2607450045f01be0608fc0d0b9fed
chobo <39817074+chobo-zz@users.noreply.github.com>
2026-08-19 09:02:52 -0700

    Add: house-robber - submission-36

---
+++ Data Structures & Algorithms/house-robber/submission-36.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        
+        first = nums[0]
+        second = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            third = max(first + nums[i], second)
+            first = second
+            second = third
+        
+        return second
