b75b8a034b66de9193d5b440ed34d32c30d7d074
chobo <39817074+chobo-zz@users.noreply.github.com>
2026-08-18 22:52:58 -0700

    Add: build-a-matrix-with-conditions - submission-16

---
+++ Data Structures & Algorithms/build-a-matrix-with-conditions/submission-16.py
@@ -0,0 +1,42 @@
+class Solution:
+    def buildMatrix(self, k: int, rowConditions: List[List[int]], colConditions: List[List[int]]) -> List[List[int]]:
+        
+        def topSort(conditions):
+            adjList = defaultdict(list)
+            indegree = { i: 0 for i in range(1, k + 1) }
+
+            for u, v in conditions:
+                adjList[u].append(v)
+                indegree[v] += 1
+            
+            q = deque([i for i in range(1, k + 1) if indegree[i] == 0])
+            res = []
+            while q:
+                node = q.popleft()
+
+                res.append(node)
+
+                for nei in adjList[node]:
+                    indegree[nei] -= 1
+                    if indegree[nei] == 0:
+                        q.append(nei)
+            return res if len(res) == len(indegree) else []
+
+
+        rowOrder = topSort(rowConditions)
+        if not rowOrder:
+            return []
+
+        colOrder = topSort(colConditions)
+        if not colOrder:
+            return []
+        
+        rti = {v:i for i, v in enumerate(rowOrder)}
+        cti = {v:i for i, v in enumerate(colOrder)}
+
+        res = [[0] * k for _ in range(k)]
+        for i in range(1, k + 1):
+            r, c = rti[i], cti[i]
+            res[r][c] = i
+        
+        return res
