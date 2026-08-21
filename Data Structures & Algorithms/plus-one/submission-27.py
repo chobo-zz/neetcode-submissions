37b5c3937a74988c9359d7c4773f184f525c160d
chobo <39817074+chobo-zz@users.noreply.github.com>
2026-08-21 04:41:48 -0700

    Add: plus-one - submission-27

---
+++ Data Structures & Algorithms/plus-one/submission-27.py
@@ -0,0 +1,15 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits.reverse()
+
+        for i in range(len(digits)):
+            if digits[i] == 9:
+                digits[i] = 0
+                if i == len(digits) - 1:
+                    digits.append(1)
+            else:
+                digits[i] += 1
+                break
+        
+        digits.reverse()
+        return digits
