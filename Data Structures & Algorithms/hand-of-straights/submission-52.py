f791871eea9734d167e481e36be14dcf26afc527
chobo <39817074+chobo-zz@users.noreply.github.com>
2026-08-20 22:03:42 -0700

    Add: hand-of-straights - submission-52

---
+++ Data Structures & Algorithms/hand-of-straights/submission-52.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+        
+        cardCount = Counter(hand)
+
+        for i in range(len(hand)):
+            card = hand[i]
+            start = card
+
+            while cardCount[start - 1]:
+                start -= 1
+            
+            while cardCount[start]:
+                for j in range(groupSize):
+                    if not cardCount[start + j]:
+                        return False
+                    cardCount[start + j] -= 1
+        
+        return True
+
+            
